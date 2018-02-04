141c1498c4d48f93c4f0c25699bfec22bdddc0be
Tanmoy Deb <tanmoydeb@gmail.com>
2018-02-04 13:05:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+ 
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>Voters Pledge</h3>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Email Address</th>
+                      <th>Mobile Number</th>
+                    </tr>
+                  </thead>
+
+            </table>
+        </div>
+    </div> <!-- /container -->
+    <?php echo "Hello"?>
+  </body>
+</html>
